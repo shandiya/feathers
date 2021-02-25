9d62981f82d2e10e267296c65a076ae44f4ca03b
shandiya <shandiyab@gmail.com>
2021-02-25 17:49:52 +1100

    adds cassowary palette

--- R/feather.R
+++ R/feather.R
@@ -10,7 +10,8 @@ feather_palettes <- list(
   oriole = c("#8a3223", "#bb5645", "#d97878", "#e2aba0", "#d0cfe9", "#a29eb8", "#6c6b75", "#b8a53f", "#93862a", "#4d4019"),
   princess_parrot = c("#7090c9", "#8cb3de", "#afbe9f", "#616020", "#6eb245", "#214917", "#cf2236", "#d683ad"),
   blue_faced_parrot_finch = c("#AA2E27", "#5B8233", "#2F561C", "#6EAD3A", "#4F52C2", "#464851"),
-  superb_fairy_wren = c("#4F3321", "#AA7853", "#D9C4A7", "#B03F05", "#020503")
+  superb_fairy_wren = c("#4F3321", "#AA7853", "#D9C4A7", "#B03F05", "#020503"),
+  cassowary = c("#BDA14D", "#3EBCB6", "#0169C4", "#153460", "#D5114E", "#A56EB6", "#4B1C57", "#09090C")
 )
 
 
